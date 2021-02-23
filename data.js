7239d3a46eb8754f05b47c4f434f284edc485e6f
hattinn <69073687+hattinn@users.noreply.github.com>
2021-02-23 22:35:51 +0900

    Update data.js

--- data.js
+++ data.js
@@ -3,6 +3,28 @@
 //
 
 //**********画像の読み込み******************
+// //数字
+// let n1 = new Image();
+// n1.src = "images/001.png";
+// let n2 = new Image();
+// n2.src = "images/001.png";
+// let n3 = new Image();
+// n3.src = "images/001.png";
+// let n4 = new Image();
+// n4.src = "images/001.png";
+// let n5 = new Image();
+// n5.src = "images/001.png";
+// let n6 = new Image();
+// n6.src = "images/001.png";
+// let n7 = new Image();
+// n7.src = "images/001.png";
+// let n8 = new Image();
+// n8.src = "images/001.png";
+// let n9 = new Image();
+// n9.src = "images/001.png";
+// let n0 = new Image();
+// n0.src = "images/001.png";
+
 //ヘッダ背景(2x)
 let headImg_2x = new Image();
 headImg_2x.src = "images/games_header@2x.png";
@@ -30,6 +52,8 @@ let icon_02_2x = new Image();/*サルアイコン02*/
 icon_02_2x.src = "images/icon_02@2x.png";
 let icon_03_2x = new Image();/*サルアイコン03*/
 icon_03_2x.src = "images/icon_03@2x.png";
+let icon_04_2x = new Image();/*サルアイコン04*/
+icon_04_2x.src = "images/icon_04@2x.png";
 // /*サルアイコン(1x)*/
 // let icon_01 = new Image();	 /*サルアイコン01*/
 // icon_01.src = "images/icon_01.png";
@@ -59,34 +83,59 @@ rope.src = "images/rope.png";
 
 
 //サル本体(2x)
-/*腕を開く*/
+//右向き
+/*開く*/
 let saru_open_2x 	= new Image();/*サル01*/
 saru_open_2x.src 	= "images/saru_01@2x.png";
 let saru_open_s_2x = new Image();/*サル01叫び*/
 saru_open_s_2x.src 	= "images/saru_01_shout@2x.png";
 let saru_open_d_2x = new Image();/*サル01ダウン*/
 saru_open_d_2x.src 	= "images/saru_01_down@2x.png";
-/*腕を開く(左向き)*/
-let saru_open_l_2x 	= new Image();/*サル01*/
-saru_open_l_2x.src 	= "images/saru_01_l@2x.png";
-let saru_open_s_l_2x = new Image();/*サル01叫び*/
-saru_open_s_l_2x.src 	= "images/saru_01_shout_l@2x.png";
-let saru_open_d_l_2x = new Image();/*サル01ダウン*/
-saru_open_d_l_2x.src 	= "images/saru_01_down_l@2x.png";
-/*腕を閉じる*/
+let saru_open_apple_2x = new Image();/*サル01アップル*/
+saru_open_apple_2x.src 	= "images/saru_01_apple@2x.png";
+/*閉じる*/
 let saru_close_2x   = new Image();/*サル02*/
 saru_close_2x.src   = "images/saru_02@2x.png";
 let saru_close_s_2x = new Image();/*サル02叫び*/
 saru_close_s_2x.src   = "images/saru_02_shout@2x.png";
 let saru_close_d_2x = new Image();/*サル02ダウン*/
 saru_close_d_2x.src = "images/saru_02_down@2x.png";
-/*腕を閉じる(左向き)*/
+let saru_close_apple_2x = new Image();/*サル02アップル*/
+saru_close_apple_2x.src 	= "images/saru_02_apple@2x.png";
+
+//左向き
+/*開く*/
+let saru_open_l_2x 	= new Image();/*サル01*/
+saru_open_l_2x.src 	= "images/saru_01_l@2x.png";
+let saru_open_s_l_2x = new Image();/*サル01叫び*/
+saru_open_s_l_2x.src 	= "images/saru_01_shout_l@2x.png";
+let saru_open_d_l_2x = new Image();/*サル01ダウン*/
+saru_open_d_l_2x.src 	= "images/saru_01_down_l@2x.png";
+let saru_open_apple_l_2x = new Image();/*サル01アップル*/
+saru_open_apple_l_2x.src 	= "images/saru_01_apple_l@2x.png";
+/*閉じる*/
 let saru_close_l_2x   = new Image();/*サル02*/
 saru_close_l_2x.src   = "images/saru_02_l@2x.png";
 let saru_close_s_l_2x = new Image();/*サル02叫び*/
 saru_close_s_l_2x.src   = "images/saru_02_shout_l@2x.png";
 let saru_close_d_l_2x = new Image();/*サル02ダウン*/
 saru_close_d_l_2x.src = "images/saru_02_down_l@2x.png";
+let saru_close_apple_l_2x = new Image();/*サル02アップル*/
+saru_close_apple_l_2x.src 	= "images/saru_02_apple_l@2x.png";
+
+//アップルダッシュ
+/*右向き*/
+let saru_apple_dash_2x = new Image();/*1*/
+saru_apple_dash_2x.src 	= "images/saru_apple_dash@2x.png";
+let saru_apple_dash_02_2x = new Image();/*2*/
+saru_apple_dash_02_2x.src 	= "images/saru_apple_dash_02@2x.png";
+/*左向き*/
+let saru_apple_dash_l_2x = new Image();/*1*/
+saru_apple_dash_l_2x.src 	= "images/saru_apple_dash_l@2x.png";
+let saru_apple_dash_02_l_2x = new Image();/*2*/
+saru_apple_dash_02_l_2x.src 	= "images/saru_apple_dash_02_l@2x.png";
+
+
 
 // //サル本体(1x)
 // /*腕を開く*/
@@ -162,5 +211,11 @@ saru_girl_l_2x.src		 = "images/saru_girl_l@2x.png";
 //GIFアニメ
 let saru_girl_d = new Image();/*サルガール*/
 saru_girl_d.src = "images/saru_girl_d.gif";
+
+
+// //ゲームオーバー画面
+// let overTitle = new Image();
+// overTitle.src = "images/gameover@2x.png";
+
 
 //*********↑↑画像の読み込みココまで↑↑******************
